d9e3bef7e8df98d7214897dae889dcb6076f342b
xorphitus <xorphitus@gmail.com>
2020-06-30 22:52:41 +0900

    add deps

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.async "1.2.603"]
+                 [com.google.api-client/google-api-client "1.23.0"]
+                 [com.google.oauth-client/google-oauth-client-jetty "1.23.0"]
+                 [com.google.apis/google-api-services-drive "v3-rev110-1.23.0"]]
   :main ^:skip-aot lgdsync.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
