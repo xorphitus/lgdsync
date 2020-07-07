5f5cc5388ab7d845c86a1017b2114a30005f3ec2
xorphitus <xorphitus@gmail.com>
2020-07-07 23:19:30 +0900

    fix FIXMEs in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lgdsync "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject lgdsync "0.1.1-SNAPSHOT"
+  :description "Lite Google Drive Sync"
+  :url "https://github.com/xorphitus/lgdsync"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
