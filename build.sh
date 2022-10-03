0387349cd0fe4835df7cbada117a56fdddf45e13
xorphitus <xorphitus@gmail.com>
2022-10-03 11:36:44 +0900

    chore: Migrate Leiningen build to Clojure CLI build

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-readonly executable_path="./target/uberjar/lgdsync"
+readonly executable_path="./target/lgdsync"
 
-lein clean
-lein uberjar
+clj -T:build clean
+clj -T:build uber
 echo "#!/usr/bin/java -jar" > "$executable_path"
-cat ./target/uberjar/lgdsync-*-standalone.jar >> "$executable_path"
+cat ./target/lgdsync-*-standalone.jar >> "$executable_path"
 chmod +x "$executable_path"
 
 echo "Created an executable file: $executable_path"
